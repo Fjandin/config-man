229d318411e9989025f863437150003ab4357cdd
René Bischoff <rene.bischoff@gmail.com>
2020-03-24 10:57:43 +0100

    AWS Secrets Manager Support

---
+++ src/lib/config/secret-manager.test.ts
@@ -0,0 +1,99 @@
+const configManJson = {
+    schema: [
+        {key: 'test1.test1a', type: 'string'},
+        {key: 'test1.test1b', type: 'number'},
+        {key: 'test1.test1c', type: 'string', nullable: true},
+        {key: 'test2', type: 'string', allowed: ['1', '2', '3']},
+        {key: 'test3', type: 'number', default: 1, allowed: [1, 2, 3]},
+        {key: 'test4', type: 'boolean', default: false}
+    ]
+}
+
+const mockGetSecretValue = jest.fn()
+const mockConfigUpdate = jest.fn()
+const mockAWS = {
+    config: {
+        update: mockConfigUpdate
+    },
+    SecretsManager: class MockedSecretsManager {
+        public getSecretValue = mockGetSecretValue
+    }
+}
+
+jest.mock('aws-sdk', () => mockAWS)
+
+import typeSecretManager from './secret-manager'
+
+describe('Config type secret manager', () => {
+    afterEach(() => {
+        jest.clearAllMocks()
+    })
+
+    it('should get config (text)', async () => {
+        mockGetSecretValue.mockImplementation((_params: any, callback: any) =>
+            callback(null, {
+                SecretString: JSON.stringify({
+                    'test1.test1a': 'foo',
+                    test3: 1
+                })
+            })
+        )
+        const result = await typeSecretManager({
+            sync: false,
+            schema: configManJson.schema,
+            secretName: 'Test',
+            region: 'eu-west-1'
+        })
+        expect(result).toEqual({'test1.test1a': 'foo', test3: 1})
+    })
+
+    it('should get config (binary)', async () => {
+        const config = JSON.stringify({'test1.test1a': 'foo', test3: 1})
+        const configBuffer = Buffer.from(config).toString('base64')
+
+        mockGetSecretValue.mockImplementation((_params: any, callback: any) =>
+            callback(null, {
+                SecretBinary: configBuffer
+            })
+        )
+        const result = await typeSecretManager({
+            sync: false,
+            schema: configManJson.schema,
+            secretName: 'Test',
+            region: 'eu-west-1'
+        })
+        expect(result).toEqual({'test1.test1a': 'foo', test3: 1})
+    })
+
+    it('should reject if config is empty', async () => {
+        mockGetSecretValue.mockImplementation((_params: any, callback: any) =>
+            callback(null, {
+                SecretString: undefined
+            })
+        )
+        await expect(
+            typeSecretManager({
+                sync: false,
+                schema: configManJson.schema,
+                secretName: 'Test',
+                region: 'eu-west-1'
+            })
+        ).rejects.toThrow('No secrets returned')
+    })
+
+    it('should reject if config is malformed', async () => {
+        mockGetSecretValue.mockImplementation((_params: any, callback: any) =>
+            callback(null, {
+                SecretString: '{{}'
+            })
+        )
+        await expect(
+            typeSecretManager({
+                sync: false,
+                schema: configManJson.schema,
+                secretName: 'Test',
+                region: 'eu-west-1'
+            })
+        ).rejects.toThrow('Unexpected token { in JSON at position 1')
+    })
+})
---
+++ src/lib/config/secret-manager.ts
@@ -0,0 +1,41 @@
+import * as AWS from 'aws-sdk'
+
+import {OptionsConfigItemOptions} from './../../index'
+
+export default function getConfigAwsDynamo(
+    options: OptionsConfigItemOptions
+): Promise<{[key: string]: any}> {
+    if (options.sync) {
+        throw new Error('This type does not support sync')
+    }
+
+    const params = {SecretId: options.secretName}
+    const awsConfig = {region: options.region}
+
+    AWS.config.update(awsConfig)
+    const client = new AWS.SecretsManager(awsConfig)
+
+    return new Promise((resolve, reject) => {
+        client.getSecretValue(params, (err, data) => {
+            if (err) {
+                return reject(err)
+            }
+            let secret: string | undefined
+            if ('SecretString' in data) {
+                secret = data.SecretString
+            } else {
+                const buffer = Buffer.from(data.SecretBinary as string, 'base64')
+                secret = buffer.toString('ascii')
+            }
+            if (!secret) {
+                return reject(new Error('No secrets returned'))
+            }
+            try {
+                const secretObj = JSON.parse(secret)
+                return resolve(secretObj)
+            } catch (e) {
+                return reject(e)
+            }
+        })
+    })
+}
